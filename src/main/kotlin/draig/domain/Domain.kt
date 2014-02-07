549b9c965c6faf73f195e83de6715eeeccfb32d5
Tim Clark <t.i.j.clark@gmail.com>
2014-02-07 22:45:01 +0000

    Add store method to repository

--- src/main/kotlin/draig/domain/Domain.kt
+++ src/main/kotlin/draig/domain/Domain.kt
@@ -16,7 +16,7 @@ abstract class Entity<T : Event>(events: List<T>)   {
 		events.forEach { handle(it) }
 	}
 
-	fun update(events: List<T>)	{
+	fun update(events: List<T>) {
 		changeList = arrayListOf<T>()
 		events.forEach { handle(it) }
 	}
@@ -39,9 +39,9 @@ abstract class Entity<T : Event>(events: List<T>)   {
 	}
 }
 
-class ChangeListExclusionStrategy(): ExclusionStrategy {
+class ChangeListExclusionStrategy() : ExclusionStrategy {
 	override fun shouldSkipField(p0: FieldAttributes?): Boolean {
-		if(p0 != null)	{
+		if (p0 != null) {
 			return p0.getName().equals("changeList")
 		}
 		return false
@@ -100,17 +100,17 @@ class SimpleStore<I : Identity, T : Event>(val stream: String) : Store<I, T>   {
 
 data class VersionedEntity<T : Event>(val version: Version, val entity: Entity<T>)
 
-abstract class Repository<I : Identity, T: Event, E : Entity<T>>(private val store: Store<I, T>)	{
-	fun find(id: I): VersionedEntity<T>?	{
+abstract class Repository<I : Identity, T : Event, E : Entity<T>>(private val store: Store<I, T>)  {
+	fun find(id: I): VersionedEntity<T>? {
 		val stream = store.stream(id)
-		if(stream.events != null)	{
+		if (stream.events != null) {
 			return VersionedEntity(stream.version, build(stream.events))
 		}
 		return null
 	}
 
-	fun store(id: I, version: Version, entity: E)	{
-		store.store(id, version, entity.changes)
+	fun store(id: I, version: Version, entity: E): StorageResult {
+		return store.store(id, version, entity.changes)
 	}
 
 	abstract fun build(events: List<T>): E
