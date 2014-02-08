13dc3d36c9d2507f231dd5de4f5f82b12a2f304e
Tim Clark <t.i.j.clark@gmail.com>
2014-02-08 19:29:43 +0000

    Completed better implementation for entity

--- src/main/kotlin/draig/core/entity/Better.kt
+++ src/main/kotlin/draig/core/entity/Better.kt
@@ -2,15 +2,23 @@ package draig.core.entity
 
 import draig.core.event.Event
 
-abstract class BetterEntity<E: Event, S>(events: List<E>)	{
+abstract class BetterEntity<E : Event, S>(events: List<E>)  {
 
-	protected var state: S = events.fold(initialState()) { s, e ->
-		handle(s, e)
-	}
+	protected var state: S = events.fold(initialState()) { s, e -> handle(s, e) }
 
-	protected fun apply(event: E)	{
+	protected fun apply(event: E) {
 		state = handle(state, event)
+		changeList.add(event)
 	}
+
+	fun update(events: List<E>) {
+		changeList = arrayListOf<E>()
+		state = events.fold(state) { s, e -> handle(s, e) }
+	}
+
+	private var changeList = arrayListOf<E>()
+	val changes: List<E>
+		get() = changeList
 
 	abstract protected fun handle(state: S, event: E): S
 	abstract protected fun initialState(): S
--- src/test/kotlin/draig/core/entity/BetterTest.kt
+++ src/test/kotlin/draig/core/entity/BetterTest.kt
@@ -10,8 +10,6 @@ abstract class BetterEvent() : Event()
 data class Wink(val message: String) : BetterEvent()
 data class Wonk(val message: String) : BetterEvent()
 
-
-
 class TestBetter(events: List<BetterEvent>) : BetterEntity<BetterEvent, ArrayList<String>>(events) {
 
 	override fun handle(state: ArrayList<String>, event: BetterEvent): ArrayList<String> {
@@ -28,25 +26,46 @@ class TestBetter(events: List<BetterEvent>) : BetterEntity<BetterEvent, ArrayLis
 		}
 	}
 
-	override fun initialState(): ArrayList<String> = arrayListOf()
+	override fun initialState() = arrayListOf<String>()
 
 	val count: Int
 		get() = state.size
 
-	fun storeWink(message: String)	{
+	fun storeWink(message: String) {
 		apply(Wink(message))
+	}
+
+	fun storeWonk(message: String) {
+		apply(Wonk("*" + message))
 	}
 }
 
-class BetterTest(): TestCase()	{
+class BetterTest() : TestCase()  {
 
-	fun testConstruction()	{
+	fun testConstruction() {
 		val b = TestBetter(listOf(Wink("1"), Wonk("2")))
 
 		assertEquals(2, b.count)
+		assertEquals(0, b.changes.size)
 
 		b.storeWink("3")
 
 		assertEquals(3, b.count)
+		assertEquals(1, b.changes.size)
+	}
+
+	fun testUpdate() {
+		val b = TestBetter(listOf(Wink("1"), Wonk("2")))
+
+		assertEquals(2, b.count)
+		assertEquals(0, b.changes.size)
+
+		b.update(listOf(Wink("3"), Wonk("4")))
+		assertEquals(4, b.count)
+		assertEquals(0, b.changes.size)
+
+		b.storeWonk("5")
+		assertEquals(5, b.count)
+		assertEquals(1, b.changes.size)
 	}
 }
